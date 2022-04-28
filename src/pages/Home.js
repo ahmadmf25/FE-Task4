ac8581e6ccf2548e2a9999dd78acda39fb638d73
ahmadmf25 <72842039+ahmadmf25@users.noreply.github.com>
2022-04-28 22:21:36 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,3 @@
-import Footer from "../components/Footer/Footer";
-import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import Movies from "../components/Movies/Movies";
 import AddMovieForm from "../components/AddMovieForm/AddMovieForm";
@@ -9,25 +7,13 @@ import { useState } from "react";
 /**
  * membuat components Main Agar Rapih untuk Hero dll
  */
-function Main () {
+function Home () {
     const [movies, setMovies] = useState(data);
     return (
         <><Hero />
         <Movies movies={movies} setMovies={setMovies}/>
         <AddMovieForm movies={movies} setMovies={setMovies}/></>
     )
-}
-/**
- * Membuat Components Home
- */
-function Home (){
-    return (
-        <div>
-            <Navbar />
-            <Main />
-            <Footer />
-        </div>
-    );
 }
 
 export default Home;
