e1bbe7167d90258a4d02809b59117b3a88e7dd74
ahmadmf25 <72842039+ahmadmf25@users.noreply.github.com>
2022-04-28 22:15:29 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*Bungkus App dengan Browser Router*/}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
